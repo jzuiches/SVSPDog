685f80a6aadbad1301750c79e717a368ed2cdf7e
Joe Zuiches <jzuiches@yahoo.com>
2016-04-18 19:56:26 -0700

    added webkit for flex css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
+
 //= require jquery_ujs
 //= require masonry/jquery.masonry
 //= require masonry/masonry.min
