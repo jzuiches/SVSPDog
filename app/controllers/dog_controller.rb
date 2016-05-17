dc90a0bdd1ae79c58fa7e13ebf6dfd81d2a59d28
Joe Zuiches <jzuiches@yahoo.com>
2016-05-16 21:36:09 -0700

    change DOB and birthday in dog controller

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -27,9 +27,11 @@ class DogController < ApplicationController
     @img = 'murphy_crop.png'
     @breed = "Golden Retreiver"
     @sex = "male"
-    @DOB = "11/28/2006"
+    birthday = "28/11/2006"
     @weight = "75"
-    @age = "9"
+    @age = age(birthday)
+    dob = Date.new(2006, 11, 28)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Eric Seelenfreund"
     @handler_img = 'ziggy.png'
 
@@ -45,9 +47,11 @@ class DogController < ApplicationController
     @img = 'boomer_card.jpg'
     @breed = "Golden Retreiver"
     @sex = "male"
-    @DOB = "06/03/2011"
+    birthday = "06/03/2011"
     @weight = "85"
-    @age = "5"
+    @age = age(birthday)
+    dob = Date.new(2011, 3, 6)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Will Paden"
     @handler_img = "will.png"
   end
@@ -57,9 +61,11 @@ class DogController < ApplicationController
     @img = 'kaya_card.jpg'
     @breed = "Belgian Malinois"
     @sex = "female"
-    @DOB = "05/09/10"
+    birthday = "05/09/10"
     @weight = "50"
-    @age = "5"
+    @age = age(birthday)
+    dob = Date.new(2010, 9, 5)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Ben Stone"
     @handler_img ="ben.png"
   end
@@ -82,9 +88,11 @@ def tucker
     @img = 'tucker_card.jpg'
     @breed = "Golden Retreiver"
     @sex = "male"
-    @DOB = "20/06/2003"
+    birthday = "20/06/2003"
     @weight = "65"
-    @age = age(@DOB)
+    @age = age(birthday)
+    dob = Date.new(2003, 6, 20)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Peter York"
     @handler_img = 'pete.png'
   end
@@ -94,9 +102,11 @@ def wylee
     @img = 'wylee_card_crop.jpg'
     @breed = "Boarder Collie"
     @sex = "male"
-    @DOB = "12/06/2007"
+    birthday = "12/06/2007"
     @weight = "45"
-    @age = age(@DOB)
+    @age = age(birthday)
+    dob = Date.new(2007, 6, 12)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Craig Noble"
     @handler_img = "craig.png"
 end
@@ -106,9 +116,11 @@ def boon
     @img = 'boon_card.jpg'
     @breed = "German Shepard"
     @sex = "female"
-    @DOB = "2003-08-23"
+    birthday = "23/08/2003"
     @weight = "65"
-    @age = age(@DOB)
+    @age = age(birthday)
+    dob = Date.new(2003, 8, 23)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @handler = "Erik Knudson"
     @handler_img = 'erik.png'
   end
@@ -117,11 +129,13 @@ def boon
   def trooper
     @name = "Trooper"
     @img = 'trooper_card.jpg'
-    @breed = ""
+    @breed = "Labrador Retreiver"
     @sex = "male"
-    @DOB = "6/1/2015"
+    birthday = "21/12/2015"
+    dob = Date.new(2015, 12, 21)
+    @DOB = dob.to_formatted_s(:long_ordinal)
     @weight = "30"
-    @age = age(@DOB)
+    @age = age(birthday)
     @handler = "Steve Selander"
     @handler_img = "steve.png"
   end
